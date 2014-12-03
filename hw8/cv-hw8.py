792b8957c10ef08e5084f38b6f7006d2b7dba471
tony056 <tony61507@gmail.com>
2014-12-03 16:56:00 +0800

    salt and pepper done

--- hw8/cv-hw8.py
+++ hw8/cv-hw8.py
@@ -19,6 +19,19 @@ def gaussianNoise(mu, sigma, amplitude, sizes, pixels):
 			result[x][y] = num + pixels[y * sizes[0] + x]
 	return result
 
+def saltAndPepper(start, end, threshold, sizes, pixels):
+	result = [[0 for x in range(0, sizes[0])] for y in range(0, sizes[1])]
+	for y in range(0, sizes[1]):
+		for x in range(0, sizes[0]):
+			num = random.uniform(start, end)
+			if num < threshold:
+				result[x][y] = 0
+			elif num > (1 - threshold):
+				result[x][y] = 255
+			else:
+				result[x][y] = pixels[y * sizes[0] + x]
+	return result
+
 
 if len(sys.argv) == 2:
 	
@@ -38,19 +51,35 @@ if len(sys.argv) == 2:
 	gaussian30_image = Image.new(source_image.mode, source_image.size, 0)
 	gaussian30_image_result = gaussian30_image.load()
 
+	saltAndPepperSmall_image = Image.new(source_image.mode, source_image.size, 0)
+	saltAndPepperSmall_image_result = saltAndPepperSmall_image.load()
+
+	saltAndPepperBig_image = Image.new(source_image.mode, source_image.size, 0)
+	saltAndPepperBig_image_result = saltAndPepperBig_image.load()
+
 	gaussianNoisesTen = gaussianNoise(0, 1, 10, [imageW, imageH], source_image_pixels)
 	gaussianNoisesThirty = gaussianNoise(0, 1, 30, [imageW, imageH], source_image_pixels)
-	
+	saltAndPepperSmall = saltAndPepper(0, 1, 0.05, [imageW, imageH], source_image_pixels)
+	saltAndPepperBig = saltAndPepper(0, 1, 0.1, [imageW, imageH], source_image_pixels)
+
 	for y in range(0, imageH):
 		for x in range(0, imageW):
 			gaussian10_image_result[x, y] = gaussianNoisesTen[x][y]
 			gaussian30_image_result[x, y] = gaussianNoisesThirty[x][y]
+			saltAndPepperSmall_image_result[x, y] = saltAndPepperSmall[x][y]
+			saltAndPepperBig_image_result[x, y] = saltAndPepperBig[x][y]
 
 	gaussian10_image.save("%s/%s.jpg" % (scriptDir, 'gaussian10'))
 	gaussian10_image.show()
 
 	gaussian30_image.save("%s/%s.jpg" % (scriptDir, 'gaussian30'))
 	gaussian30_image.show()
+
+	saltAndPepperSmall_image.save("%s/%s.jpg" % (scriptDir, 'saltAndPepper_0.05'))
+	saltAndPepperSmall_image.show()
+
+	saltAndPepperBig_image.save("%s/%s.jpg" % (scriptDir, 'saltAndPepper_0.1'))
+	saltAndPepperBig_image.show()
 
 
 else:
