486d0365c0830fa35232c1aaffef7634b62b9133
tony056 <tony61507@gmail.com>
2014-12-04 15:13:27 +0800

    box filter done

--- hw8/cv-hw8.py
+++ hw8/cv-hw8.py
@@ -32,6 +32,51 @@ def saltAndPepper(start, end, threshold, sizes, pixels):
 				result[x][y] = pixels[y * sizes[0] + x]
 	return result
 
+def boxFilter(sizes, boxSizes, pixels):
+	extend_pixels = extend(sizes, boxSizes, pixels)
+	result = [[0 for x in range(0, sizes[0])] for y in range(0, sizes[1])]
+	for y in range(0, sizes[1]):
+		for x in range(0, sizes[0]):
+			average = averageOfNeighbor(x, y, boxSizes, extend_pixels)
+			result[x][y] = average
+	return result
+
+def averageOfNeighbor(x, y, boxSizes, pixels):
+	num = 0
+	for localY in range(y - (boxSizes[1] / 2), y + (boxSizes[1] / 2 + 1)):
+		for localX in range(x - (boxSizes[0] / 2), x + (boxSizes[0] / 2 + 1)):
+			num += pixels[localX][localY]
+	return int(num / (boxSizes[0] * boxSizes[1]))  
+
+def medianFilter(sizes, neighborSizes, pixels):
+	extend_pixels = extend(sizes, neighborSizes, pixels)
+	result = [[0 for x in range(0, sizes[0])] for y in range(0, sizes[1])]
+	for y in range(0, sizes[1]):
+		for x in range(0, sizes[0]):
+			median = medianOfNeighbor(x, y, extend_pixels)
+			result[x][y] = median
+	return result
+
+def medianOfNeighbor(x, y, neighborSizes, pixels):
+	num = 0
+	local = []
+	for localY in range(y - (neighborSizes[1] / 2), y + (neighborSizes[1] / 2 + 1)):
+		for localX in range(x - (neighborSizes[0] / 2), x + (neighborSizes[0] / 2 + 1)):
+			local.append(pixels[localX][localY])
+	sortedList = sorted(local)
+	return sortedList[int(len(sortedList) / 2)]
+
+def extend(sizes, extendSizes, pixels):
+	extendNum = int(extendSizes[0] / 2) * 2
+	extend_pixels = [[0 for x in range(0, sizes[0] + extendNum)] for y in range(0, sizes[1] + extendNum)]
+	startPoint = int(extendNum / 2)
+	for y in range(startPoint, sizes[1] + startPoint):
+		for x in range(startPoint, sizes[0] + startPoint):
+			extend_pixels[x][y] = pixels[x - startPoint][y - startPoint]
+	return extend_pixels
+
+# def toImage(imageList):
+
 
 if len(sys.argv) == 2:
 	
@@ -57,10 +102,19 @@ if len(sys.argv) == 2:
 	saltAndPepperBig_image = Image.new(source_image.mode, source_image.size, 0)
 	saltAndPepperBig_image_result = saltAndPepperBig_image.load()
 
+	boxFilterSmall_image = Image.new(source_image.mode, source_image.size, 0)
+	boxFilterSmall_image_result = boxFilterSmall_image.load()
+
+	boxFilterBig_image = Image.new(source_image.mode, source_image.size, 0)
+	boxFilterBig_image_result = boxFilterBig_image.load()
+
 	gaussianNoisesTen = gaussianNoise(0, 1, 10, [imageW, imageH], source_image_pixels)
 	gaussianNoisesThirty = gaussianNoise(0, 1, 30, [imageW, imageH], source_image_pixels)
 	saltAndPepperSmall = saltAndPepper(0, 1, 0.05, [imageW, imageH], source_image_pixels)
 	saltAndPepperBig = saltAndPepper(0, 1, 0.1, [imageW, imageH], source_image_pixels)
+
+	boxFilterSmall = boxFilter(source_image.size, [3, 3], gaussianNoisesTen)
+	boxFilterBig = boxFilter(source_image.size, [5, 5], gaussianNoisesTen)
 
 	for y in range(0, imageH):
 		for x in range(0, imageW):
@@ -68,19 +122,26 @@ if len(sys.argv) == 2:
 			gaussian30_image_result[x, y] = gaussianNoisesThirty[x][y]
 			saltAndPepperSmall_image_result[x, y] = saltAndPepperSmall[x][y]
 			saltAndPepperBig_image_result[x, y] = saltAndPepperBig[x][y]
+			boxFilterSmall_image_result[x, y] = boxFilterSmall[x][y]
+			boxFilterBig_image_result[x, y] = boxFilterBig[x][y]
 
 	gaussian10_image.save("%s/%s.jpg" % (scriptDir, 'gaussian10'))
-	gaussian10_image.show()
+	# gaussian10_image.show()
 
 	gaussian30_image.save("%s/%s.jpg" % (scriptDir, 'gaussian30'))
-	gaussian30_image.show()
+	# gaussian30_image.show()
 
 	saltAndPepperSmall_image.save("%s/%s.jpg" % (scriptDir, 'saltAndPepper_0.05'))
-	saltAndPepperSmall_image.show()
+	# saltAndPepperSmall_image.show()
 
 	saltAndPepperBig_image.save("%s/%s.jpg" % (scriptDir, 'saltAndPepper_0.1'))
-	saltAndPepperBig_image.show()
+	# saltAndPepperBig_image.show()
 
+	boxFilterSmall_image.save("%s/%s.jpg" % (scriptDir, 'boxForGaussian'))
+	boxFilterSmall_image.show()
+
+	boxFilterBig_image.save("%s/%s.jpg" % (scriptDir, 'boxForGaussianBig'))
+	boxFilterBig_image.show()
 
 else:
 	print 'please enter the path of source image!'
